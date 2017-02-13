3f52e163c7bf85581f61151cea99af53fbef100a
serafettinserafettin <serafettin.serafettin@gmail.com>
2017-02-13 19:13:31 +0200

    Update Base.java

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -30,8 +30,9 @@ import org.junit.internal.AssumptionViolatedException;
 class Base {
 
     protected void run() {
-        //double r = Math.random();
-        double r = 1;
+        
+        double r = Math.random();
+        
         if (r < 0.1) {
             fail("oops");
         } else if (r < 0.2) {
